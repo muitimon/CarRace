5a591013e6b68b900a0b00e4127c6b52e29cbf1f
muitimon <g1444431@gmail.com>
2016-07-14 00:46:16 +0900

    StopDrive実装
    相手よりスコアを取っていて相手が2の旗へ向かっている時止まる
    CheckOtherMoveからCheckOtherMoveFlagへ名称変更しました

--- d/AIController.java
+++ d/AIController.java
@@ -1,16 +1,21 @@
 package simplerace.d;
 import simplerace.*;
 import java.lang.Math;
+import java.util.Scanner;
 
 public class AIController implements Controller, Constants {
   static SelectFlag selectflag = new SelectFlag();
+  static CheckOtherMoveFlag checkOtherMoveFlag = new CheckOtherMoveFlag();
 
   public void reset() {}
   public int control (SensorModel inputs) {
 	int command = neutral;
-
+	// Scanner sc = new Scanner(System.in);
+	// String s = sc.nextLine();
+	
 	command = AIController.NormalDrive(inputs);
-
+	
+	command = AIController.StopDrive(inputs,command);
 	//        System.out.print(command);
 	return command;
   }
@@ -18,7 +23,7 @@ public class AIController implements Controller, Constants {
   public static int NormalDrive(SensorModel inputs){
 	int command = neutral;
 	double speed = inputs.getSpeed();
-	double nextwayangle = selectflag.Check(inputs)[0];;
+	double nextwayangle = selectflag.Check(inputs)[0];
 	double nextwaydistance = selectflag.Check(inputs)[1];
 
 	if(-6.0 < speed && speed < 6.0){
@@ -150,4 +155,49 @@ public class AIController implements Controller, Constants {
 	return command;
   }
 
+  private static final int nextFlag = 1;
+  private static final int nextnextFlag = 2;
+  public static int StopDrive(SensorModel inputs, int preCommand){
+	int command       = preCommand;
+
+	//自分,相手のスコア 
+	double myScore    = inputs.getScore();
+	double otherScore = inputs.getOtherVehicleScore();
+
+	if(myScore > otherScore){
+	  double nextwaydistance = inputs.getDistanceToNextWaypoint();
+	  //System.out.println(myNextToCarDistance);
+	  int otherMoveFlag = checkOtherMoveFlag.Check(inputs);
+	  System.out.println("otherMoveFlag " + otherMoveFlag);
+	  double speed = inputs.getSpeed();
+	  double nextwayangle = inputs.getAngleToNextWaypoint();
+	  if(otherMoveFlag == nextnextFlag){
+		System.out.println("myScore " + myScore + " otherScore " + otherScore);
+		
+		if(nextwaydistance < 0.05){
+		System.out.println("OK");
+		  if(0.001 < speed){
+			if(0 < nextwayangle){
+			  command = backwardleft;
+			}
+			else{
+			  command = backwardright;
+			}
+		  }
+		  else if(speed < -0.001){
+			if(0 < nextwayangle){
+			  command = forwardleft;
+			}
+			else{
+			  command = forwardright;
+			}
+		  }
+		}
+	  }
+	 
+	}
+	//System.out.println(otherMoveFlag);
+	return command;
+  }
+  
 }
